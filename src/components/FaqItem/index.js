8a21997f3c98683d4b7f8d275fb7c8bda84a49c0
Siva Dinesh K <ksd.ccbp4@gmail.com>
2024-02-17 07:18:26 +0000

    passed tests

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,29 +1,49 @@
 import './index.css'
 
 const FaqItem = props => {
-  const {faqDetails, showFaq, isIdSame} = props
+  const {faqDetails, showFaq, isIdSame, hideFaq} = props
   const {id, questionText, answerText} = faqDetails
-
-  const imageUrl = isIdSame
-    ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
-    : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
-  const imgAlt = isIdSame ? 'minus' : 'plus'
 
   const onshowFaqClicked = () => {
     showFaq(id)
   }
 
+  const onHideFaqClicked = () => {
+    hideFaq(id)
+  }
+
   return (
     <li className="faq-item">
       <div className="question-card">
-        <p className="faq-question">{questionText}</p>
-        <button
-          className="show-hide-btn"
-          type="button"
-          onClick={onshowFaqClicked}
-        >
-          <img src={imageUrl} alt={imgAlt} className="show-hide-icon" />
-        </button>
+        <h2 className="faq-question">{questionText}</h2>
+        <div>
+          {isIdSame && (
+            <button
+              className="show-hide-btn"
+              type="button"
+              onClick={onHideFaqClicked}
+            >
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png"
+                alt="minus"
+                className="show-hide-icon"
+              />
+            </button>
+          )}
+          {!isIdSame && (
+            <button
+              className="show-hide-btn"
+              type="button"
+              onClick={onshowFaqClicked}
+            >
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png"
+                alt="plus"
+                className="show-hide-icon"
+              />
+            </button>
+          )}
+        </div>
       </div>
       {isIdSame && (
         <>
--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -1,1 +1,55 @@
-// Write your code here.
+import {Component} from 'react'
+
+import FaqItem from '../FaqItem'
+import './index.css'
+
+class Faqs extends Component {
+  state = {isAnswerVisible: false, idList: []}
+
+  showFaq = id => {
+    const {idList} = this.state
+    this.setState({
+      isAnswerVisible: true,
+      idList: [...idList, id],
+    })
+  }
+
+  hideFaq = hideId => {
+    const {idList} = this.state
+    const newList = idList.filter(id => id !== hideId)
+    this.setState({isAnswerVisible: false, idList: newList})
+  }
+
+  renderFaqItem = () => {
+    const {faqsList} = this.props
+    const {isAnswerVisible, idList} = this.state
+    console.log(isAnswerVisible)
+
+    return (
+      <ul className="faq-item-list">
+        {faqsList.map(faq => (
+          <FaqItem
+            key={faq.id}
+            faqDetails={faq}
+            showFaq={this.showFaq}
+            isIdSame={idList.includes(faq.id)}
+            hideFaq={this.hideFaq}
+          />
+        ))}
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <div className="faqs-container">
+          <h1 className="faq-title">FAQs</h1>
+          {this.renderFaqItem()}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Faqs
